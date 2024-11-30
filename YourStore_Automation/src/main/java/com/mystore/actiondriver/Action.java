341c9ebccb592523c7de1bae3a7092a5ff2721e3
Pritamh <pritam.horo143@gmail.com>
2024-11-30 16:03:31 +0530

    action class

--- YourStore_Automation/src/main/java/com/mystore/actiondriver/Action.java
+++ YourStore_Automation/src/main/java/com/mystore/actiondriver/Action.java
@@ -1,6 +1,12 @@
 package com.mystore.actiondriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Action 
 {
-
+	public void actions(WebDriver driver) 
+	{
+		Actions action=new Actions(driver);
+	}
 }

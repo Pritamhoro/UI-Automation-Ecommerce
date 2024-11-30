262f667e155a0585e68ac574f6c03ab876f1d717
Pritamh <pritam.horo143@gmail.com>
2024-11-30 12:43:40 +0530

    Setup baseClass and dependencies

---
+++ YourStore_Automation/src/main/java/com/mystore/basepage/BaseClass.java
@@ -0,0 +1,35 @@
+package com.mystore.basepage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass 
+{
+	public static Properties prop;
+	public static WebDriver driver;
+	@BeforeTest
+	public void loadconfig() 
+	{
+		try
+		{
+			prop=new Properties();
+			System.out.println("Super constructor invoked");
+			FileInputStream ip=new FileInputStream(System.getProperty("user.dir")+"/YourStore_Automation/Configuration/Config.properties");
+			prop.load(ip);
+			System.out.println("driver :"+ driver);
+		}
+		catch(FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		catch(IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
